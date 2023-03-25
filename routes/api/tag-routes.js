dd3a2debb51b92ec09e8299b397f0ec87ced43ee
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-25 18:58:50 -0400

    Coded post api route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => { 
+  res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
