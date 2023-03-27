5e17726894773860a3f08474348284b046b4f8d1
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-27 14:56:51 -0400

    Finsihed get one endpoint

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,6 +26,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category, {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
