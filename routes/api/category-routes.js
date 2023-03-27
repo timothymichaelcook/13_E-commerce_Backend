e401b9b22e0769b2653a46a1b9b8f29ba3b1e2dc
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-27 14:16:54 -0400

    Figuring out node_modules issue

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,14 +44,48 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name 
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+   }
+  )
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'Category was not found.'})
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
