cee4a4fa1dc1a6cc50cf124dd8ffa646ecf4d0f2
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-29 21:37:28 -0400

    Changed connection.js and .gitignore files. Used launch.json to debug program.

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
+
 const Sequelize = require('sequelize');
+console.log(process.env);
+
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
